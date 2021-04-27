a52317f55e92f7ea1726bf5d48d3d6e1b887cd55
skovbakke <78202835+skovbakke@users.noreply.github.com>
2021-04-27 17:31:12 +0000

    Remove debug messages

--- scripts/prepare_systemd_network.sh
+++ scripts/prepare_systemd_network.sh
@@ -10,12 +10,9 @@ $(<$TEMPLATES_DIR/10-systemd-network.network )
 EOF
 " > $OUTPUT_DIR/10-systemd-network.network
 
-echo "[DEBUG] finished networking at DNS"
-echo "CONFIGURE_DNS is $CONFIGURE_DNS"
 # Configure the DNS by moving the static template file 
 if [[ $CONFIGURE_DNS = "true" ]]
 then
-    echo "[DEBUG] configuring DNS"
     if [ -z "$CLUSTER_DNS" ]
     then
         echo "[error] CLUSTER_DNS is required when CONFIGURE_DNS is true"
