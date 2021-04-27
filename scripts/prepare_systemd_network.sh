73345b95ed3b225aff1c486d9754df87d6fc9e4d
skovbakke <78202835+skovbakke@users.noreply.github.com>
2021-04-27 17:30:30 +0000

    Added DNS options for the config file

--- scripts/prepare_systemd_network.sh
+++ scripts/prepare_systemd_network.sh
@@ -10,5 +10,19 @@ $(<$TEMPLATES_DIR/10-systemd-network.network )
 EOF
 " > $OUTPUT_DIR/10-systemd-network.network
 
+echo "[DEBUG] finished networking at DNS"
+echo "CONFIGURE_DNS is $CONFIGURE_DNS"
 # Configure the DNS by moving the static template file 
-cp $TEMPLATES_DIR/resolved.conf $WORKING_DIR/etc/systemd/resolved.conf
+if [[ $CONFIGURE_DNS = "true" ]]
+then
+    echo "[DEBUG] configuring DNS"
+    if [ -z "$CLUSTER_DNS" ]
+    then
+        echo "[error] CLUSTER_DNS is required when CONFIGURE_DNS is true"
+        exit 1
+    fi
+    eval "cat <<EOF
+$(<$TEMPLATES_DIR/resolved.conf)
+EOF
+" > $WORKING_DIR/etc/systemd/resolved.conf
+fi
