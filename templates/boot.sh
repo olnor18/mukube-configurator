ad5be0acbe1e60c031304c9d6105068d8fa26f2b
skovbakke <78202835+skovbakke@users.noreply.github.com>
2021-06-04 11:02:16 +0000

    hardcode the ssh-tunnel to the harbor registry into the hosts file

--- templates/boot.sh
+++ templates/boot.sh
@@ -14,6 +14,7 @@ case $NODE_TYPE in
         # General setup
         hostnamectl set-hostname $NODE_NAME
         echo  "127.0.1.1	$NODE_NAME" >> /etc/hosts
+        echo  "192.168.1.200	harbor.sc.garagen" >> /etc/hosts
         ;;&
     master*)
         # masters setup
