c49f328a9913df6d34d115802d3c8d0606a02a79
Jacob Crawford <jacobcrawford1@hotmail.com>
2021-06-30 07:22:46 +0000

    Removed haproxy setup

--- scripts/prepare_master_HA.sh
+++ scripts/prepare_master_HA.sh
@@ -10,7 +10,6 @@ export APISERVER_DEST_PORT=$NODE_CONTROL_PLANE_PORT
 export APISERVER_VIP=$NODE_CONTROL_PLANE_VIP
 
 mkdir -p $WORKING_DIR/etc/keepalived
-mkdir -p $WORKING_DIR/etc/haproxy
 mkdir -p $WORKING_DIR/root/ha
 
 # Fill in check_apiserver.sh
@@ -21,24 +20,6 @@ EOF
 " > $WORKING_DIR/etc/keepalived/check_apiserver.sh
 
 VIP_IPS=$MASTER_VIP_CLUSTER_IPS
-
-# Fill in haproxy.cfg 
-eval "cat <<EOF
-$(<$TEMPLATES_DIR/haproxy.cfg )
-EOF
-" > $WORKING_DIR/etc/haproxy/haproxy.cfg 
-
-# MAKE HOST_IP list
-IFS=, read -ra IPS <<< "$VIP_IPS"
-for ((i=0; i<${#IPS[@]}; i++)); do
-    echo -e "\t\tserver MASTER_VIP$i ${IPS[i]}:6443 check" >> $WORKING_DIR/etc/haproxy/haproxy.cfg
-done
-
-# Fill in haproxy.yaml
-eval "cat <<EOF
-$(<$TEMPLATES_DIR/haproxy.yaml)
-EOF
-" > $1/root/ha/haproxy.yaml
 
 # Fill in keepalived.yaml
 eval "cat <<EOF
