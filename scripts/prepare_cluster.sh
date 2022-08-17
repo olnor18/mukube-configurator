olnor18/mukube-configurator

commit 60407ac3639afbe517418100b7d030b247c0f44a

Remove helm runtime dependency
We want low coupling between between the OS and mukube-configurator, so
changes in mukube-configurator doesn't require changing the OS.

With this change charts are templated as part of the build process and
the OS just executes `kubectl apply -f $FILE`, and changing from
Helm/ArgoCD to ex: Flux would just require changing mukube-configurator.

scripts/prepare_cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+shopt -s extglob
 OUTPUT_DIR=$1
 VARIABLES=$2
 source $VARIABLES
@@ -86,6 +87,7 @@ for ((i=0; i<${#MASTERS[@]}; i++)); do
     export NODE_NAME=master$i
     export MASTER_PROXY_PRIORITY=$(expr 100 - $i)
     OUTPUT_DIR_MASTER=$OUTPUT_DIR/$CLUSTER_NAME-master$i
+    mkdir -p $OUTPUT_DIR_MASTER
 
     if [ $i = 0 ];
     then
@@ -97,35 +99,53 @@ for ((i=0; i<${#MASTERS[@]}; i++)); do
     fi
 
     OUTPUT_PATH_CONF=$OUTPUT_DIR_MASTER/mukube_init_config
-    mkdir -p $OUTPUT_DIR_MASTER
 
     mkdir -p $OUTPUT_DIR_MASTER/etc/containers/registries.conf.d/
     cp templates/registries.conf $OUTPUT_DIR_MASTER/etc/containers/
     cp "$mirrors_conf" $OUTPUT_DIR_MASTER/etc/containers/registries.conf.d/mirrors.conf
-    OUTPUT_PATH_VALUES="$OUTPUT_DIR_MASTER/root/helm-charts/values"
-    mkdir -p "$OUTPUT_PATH_VALUES"
-    eval "echo \"$(<templates/nidhogg-lb.yaml)\"" >> "$OUTPUT_PATH_VALUES/nidhogg.yaml"
-    if [ "$EXTERNAL_DNS_ENABLED" = "true" ]; then
-        eval "echo \"$(<templates/nidhogg-external-dns.yaml)\"" >> "$OUTPUT_PATH_VALUES/nidhogg.yaml"
-    fi
-    if [ "$PROXY_ENABLED" = "true" ]; then
-        mkdir -p "$OUTPUT_DIR_MASTER/etc/sysconfig"
-        cp "$crio_sysconfig" "$OUTPUT_DIR_MASTER/etc/sysconfig/crio"
-        eval "echo \"$(<templates/nidhogg-proxy.yaml)\"" >> "$OUTPUT_PATH_VALUES/nidhogg.yaml"
-        if [ -n "$PROXY_CA_FILE" ]; then
-            mkdir -p "$OUTPUT_DIR_MASTER/etc/crio/ssl/"
-            cp "$PROXY_CA_FILE" "$OUTPUT_DIR_MASTER/etc/crio/ssl/root.pem"
-            chmod 444 "$OUTPUT_DIR_MASTER/etc/crio/ssl/root.pem"
-            cat templates/nidhogg-proxy-ca.yaml >> "$OUTPUT_PATH_VALUES/nidhogg.yaml"
+
+    if [ $i = 0 ]; then
+        OUTPUT_PATH_VALUES="$(mktemp -d)"
+        trap "rm -r \"$OUTPUT_PATH_VALUES\"" EXIT
+        mkdir -p "$OUTPUT_PATH_VALUES"
+        eval "echo \"$(<templates/nidhogg-lb.yaml)\"" >> "$OUTPUT_PATH_VALUES/nidhogg.yaml"
+        if [ "$EXTERNAL_DNS_ENABLED" = "true" ]; then
+            eval "echo \"$(<templates/nidhogg-external-dns.yaml)\"" >> "$OUTPUT_PATH_VALUES/nidhogg.yaml"
         fi
-    fi
-    OUTPUT_PATH_VALUES_OVERRIDE="$OUTPUT_DIR/../root/helm-charts/values/"
-    if [ -f "$OUTPUT_PATH_VALUES_OVERRIDE/nidhogg.yaml" ]; then
-        # https://github.com/mikefarah/yq
-        # https://mikefarah.gitbook.io/yq/v/v4.x/operators/reduce#merge-all-yaml-files-together
-        yq eval-all '. as $item ireduce ({}; . * $item )' "$OUTPUT_PATH_VALUES/nidhogg.yaml" "$OUTPUT_PATH_VALUES_OVERRIDE/nidhogg.yaml" > "$OUTPUT_PATH_VALUES/nidhogg.yaml.tmp"
-        mv "$OUTPUT_PATH_VALUES/nidhogg.yaml"{.tmp,}
-    fi
+        if [ "$PROXY_ENABLED" = "true" ]; then
+            mkdir -p "$OUTPUT_DIR_MASTER/etc/sysconfig"
+            cp "$crio_sysconfig" "$OUTPUT_DIR_MASTER/etc/sysconfig/crio"
+            eval "echo \"$(<templates/nidhogg-proxy.yaml)\"" >> "$OUTPUT_PATH_VALUES/nidhogg.yaml"
+            if [ -n "$PROXY_CA_FILE" ]; then
+                mkdir -p "$OUTPUT_DIR_MASTER/etc/crio/ssl/"
+                cp "$PROXY_CA_FILE" "$OUTPUT_DIR_MASTER/etc/crio/ssl/root.pem"
+                chmod 444 "$OUTPUT_DIR_MASTER/etc/crio/ssl/root.pem"
+                cat templates/nidhogg-proxy-ca.yaml >> "$OUTPUT_PATH_VALUES/nidhogg.yaml"
+            fi
+        fi
+        OUTPUT_PATH_VALUES_OVERRIDE="$OUTPUT_DIR/../root/helm-charts/values/"
+        if [ -f "$OUTPUT_PATH_VALUES_OVERRIDE/nidhogg.yaml" ]; then
+            # https://github.com/mikefarah/yq
+            # https://mikefarah.gitbook.io/yq/v/v4.x/operators/reduce#merge-all-yaml-files-together
+            yq eval-all '. as $item ireduce ({}; . * $item )' "$OUTPUT_PATH_VALUES/nidhogg.yaml" "$OUTPUT_PATH_VALUES_OVERRIDE/nidhogg.yaml" > "$OUTPUT_PATH_VALUES/nidhogg.yaml.tmp"
+            mv "$OUTPUT_PATH_VALUES/nidhogg.yaml"{.tmp,}
+        fi
+
+        rm -f "$OUTPUT_DIR_MASTER/root/crds.yaml"
+        for FILE in build/root/helm-charts/!(values); do
+            release=$(echo $FILE | cut -f4 -d/ | cut -f1 -d#)
+            namespace=$(echo $FILE | cut -f4 -d/ | cut -f2 -d#)
+            values_file="$OUTPUT_PATH_VALUES/nidhogg.yaml"
+            mkdir -p "$OUTPUT_DIR_MASTER/root"
+            if [ -f "$values_file" ]; then
+                helm template --skip-tests --include-crds -f "$values_file" -n $namespace $release $FILE > "$OUTPUT_DIR_MASTER/root/manifest-$namespace.yaml"
+            else
+                helm template --skip-tests --include-crds -n $namespace $release $FILE > "$OUTPUT_DIR_MASTER/root/manifest-$namespace.yaml"
+            fi
+            yq e 'select(.kind == "CustomResourceDefinition")' "$OUTPUT_DIR_MASTER/root/manifest-$namespace.yaml" >> "$OUTPUT_DIR_MASTER/root/crds.yaml"
+        done
+     fi
+
     ./scripts/prepare_master_config.sh $OUTPUT_PATH_CONF $VARIABLES
     ./scripts/prepare_systemd_network.sh $OUTPUT_DIR_MASTER templates
     ./scripts/prepare_master_HA.sh $OUTPUT_DIR_MASTER templates

scripts/build_cluster.sh
@@ -11,11 +11,6 @@ for node in $NODEDIR/*; do
 	echo "[INFO] Archiving $node --> $archive_path"
 	tar -cf $archive_path -C $node .
 
-	#Pack the images and helm charts for master nodes
-	if [[ $node == *"master"* ]]; then
-		echo "	* adding helm-charts"
-		tar -rf $archive_path -C build root/helm-charts --exclude **/.empty --exclude root/helm-charts/values/nidhogg.yaml
-	fi
 	tmp="$(mktemp -d)"
 	cp "$archive_path" "$tmp/config.tar"
 	mkfs.ext4 -L config -d "$tmp" "${archive_path%%.tar}.ext4" 1M

templates/boot.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-shopt -s extglob
-
 # Load all the variables from the config.yaml file to variables
 source mukube_init_config
 
@@ -55,15 +53,11 @@ case $NODE_TYPE in
         # Need to export KUBECONFIG for helm to contact the api-server
         export KUBECONFIG=/etc/kubernetes/admin.conf
         echo "Installing included helm charts"
-        for FILE in /root/helm-charts/!(values); do
-            release=$(echo $FILE | cut -f4 -d/ | cut -f1 -d#)
-            namespace=$(echo $FILE | cut -f4 -d/ | cut -f2 -d#)
-            values_file="/root/helm-charts/values/$release.yaml"
-            if [ -f "$values_file" ]; then
-                helm install --create-namespace -f "$values_file" -n $namespace $release $FILE
-            else
-                helm install --create-namespace -n $namespace $release $FILE
-            fi
+        kubectl apply -f /root/crds.yaml
+        for FILE in /root/manifest-*.yaml; do
+            NAMESPACE="$(cut -f2- -d - <<< "$FILE" | cut -f1 -d .)"
+            kubectl create namespace "$NAMESPACE" --dry-run=client -o yaml | kubectl apply -f -
+            kubectl apply -n "$NAMESPACE" -f "$FILE"
         done
         ;;&
     master-join)

commit 82b6bb56da5799cd329e6a1dec731d36bf1e7ac2

Fix proxy configuration only being applied on the first node
The first node is in charge of initializing the cluster and in 60407ac
the logic was refactored, so the "initialization" files are only stored
where needed (the first node). Unfortunately it was missed, that the
proxy configuration must be applied on every node.

Fixes: 60407ac ("Remove helm runtime dependency")

scripts/prepare_cluster.sh
@@ -107,6 +107,17 @@ for ((i=0; i<${#MASTERS[@]}; i++)); do
     cp templates/registries.conf $OUTPUT_DIR_MASTER/etc/containers/
     cp "$mirrors_conf" $OUTPUT_DIR_MASTER/etc/containers/registries.conf.d/mirrors.conf
 
+
+    if [ "$PROXY_ENABLED" = "true" ]; then
+        mkdir -p "$OUTPUT_DIR_MASTER/etc/sysconfig"
+        cp "$crio_sysconfig" "$OUTPUT_DIR_MASTER/etc/sysconfig/crio"
+        if [ -n "$PROXY_CA_FILE" ]; then
+            mkdir -p "$OUTPUT_DIR_MASTER/etc/crio/ssl/"
+            cp "$PROXY_CA_FILE" "$OUTPUT_DIR_MASTER/etc/crio/ssl/root.pem"
+            chmod 444 "$OUTPUT_DIR_MASTER/etc/crio/ssl/root.pem"
+        fi
+    fi
+
     if [ $i = 0 ]; then
         OUTPUT_PATH_VALUES="$(mktemp -d)"
         trap "rm -r \"$OUTPUT_PATH_VALUES\"" EXIT
@@ -115,14 +126,10 @@ for ((i=0; i<${#MASTERS[@]}; i++)); do
         if [ "$EXTERNAL_DNS_ENABLED" = "true" ]; then
             eval "echo \"$(<templates/nidhogg-external-dns.yaml)\"" >> "$OUTPUT_PATH_VALUES/nidhogg.yaml"
         fi
+
         if [ "$PROXY_ENABLED" = "true" ]; then
-            mkdir -p "$OUTPUT_DIR_MASTER/etc/sysconfig"
-            cp "$crio_sysconfig" "$OUTPUT_DIR_MASTER/etc/sysconfig/crio"
             eval "echo \"$(<templates/nidhogg-proxy.yaml)\"" >> "$OUTPUT_PATH_VALUES/nidhogg.yaml"
             if [ -n "$PROXY_CA_FILE" ]; then
-                mkdir -p "$OUTPUT_DIR_MASTER/etc/crio/ssl/"
-                cp "$PROXY_CA_FILE" "$OUTPUT_DIR_MASTER/etc/crio/ssl/root.pem"
-                chmod 444 "$OUTPUT_DIR_MASTER/etc/crio/ssl/root.pem"
                 cat templates/nidhogg-proxy-ca.yaml >> "$OUTPUT_PATH_VALUES/nidhogg.yaml"
             fi
         fi